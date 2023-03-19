eb1cc4a8559ee1717830755fe876ac046ff1091d
Alex Chiew <alex.chiewyz@gmail.com>
2023-03-19 14:04:56 +0800

    Track user's last login date and time

---
+++ app/Listeners/LogSuccessfulLogin.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Auth\Events\Login;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class LogSuccessfulLogin
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(Login $event): void
+    {
+        $event->user->last_login = now();
+        $event->user->save();
+    }
+}
