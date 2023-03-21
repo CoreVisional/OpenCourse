400fe1c6e20e693c81f35dc67447a98c9f54d703
Alex Chiew <alex.chiewyz@gmail.com>
2023-03-22 00:16:40 +0800

    Make specialization_id foreign key to be nullable

--- database/migrations/2023_02_06_093321_create_courses_table.php
+++ database/migrations/2023_02_06_093321_create_courses_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('courses', function (Blueprint $table) {
             $table->bigIncrements('course_id');
-            $table->foreignId('specialization_id')
+            $table->foreignId('specialization_id')->nullable()
                 ->constrained('specializations', 'specialization_id')
                 ->cascadeOnUpdate()
                 ->cascadeOnDelete();
