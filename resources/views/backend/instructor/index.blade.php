5340e4deed9dd0e3e442fd7848a4545435d91d98
Alex Chiew <alex.chiewyz@gmail.com>
2023-03-22 02:00:22 +0800

    Add index page on instructor dashboard for instructor user

---
+++ resources/views/backend/instructor/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.backend.main')
+
+@section('title', 'Instructor Dashboard | OpenCourse')
+
+@section('content')
+
+    {{-- TODO: Populate admin home index view --}}
+
+@endsection
