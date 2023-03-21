2e1a5b8f6bed357f72a9b4efe170e4400267ba8f
Alex Chiew <alex.chiewyz@gmail.com>
2023-03-22 01:53:10 +0800

    Add index page for courses tab for org_admin user

--- resources/views/backend/org_admin/courses/index.blade.php
+++ resources/views/backend/org_admin/courses/index.blade.php
@@ -7,7 +7,7 @@
     <div class="container-xxl flex-grow-1 container-p-y">
         <div class="d-sm-flex align-items-center justify-content-between mb-4">
             <h4 class="fw-bold mb-3 mt-3" style="margin-bottom: 0;">Courses</h4>
-            <a href="" class="btn btn-primary">
+            <a href="{{ route('courses.create') }}" class="btn btn-primary">
                 <i class="fa-solid fa-plus me-2"></i>New Course
             </a>
         </div>
@@ -20,12 +20,40 @@
                         <thead>
                             <tr>
                                 <th>{{ __('Course Name') }}</th>
+                                <th>{{ __('Course Thumbnail') }}</th>
                                 <th>{{ __('Duration') }}</th>
-                                <th>{{ __('Instructor') }}</th>
                                 <th>{{ __('Status') }}</th>
                                 <th>{{ __('Actions') }}</th>
                             </tr>
                         </thead>
+                        <tbody>
+                            @foreach ($courses as $course)
+                                <tr>
+                                    <td>{{ $course->course_name }}</td>
+                                    <td>
+                                        <img src="{{ asset('storage/' . $course->course_image) }}" alt="Course Thumbnail" width="50">
+                                    </td>
+                                    <td>{{ $course->duration }} days</td>
+                                    <td>
+                                        @if (!$course->is_active)
+                                        <span class="badge bg-label-danger">Disabled</span>
+                                        @else
+                                            <span class="badge bg-label-success">Active</span>
+                                        @endif
+                                    </td>
+                                    <td>
+                                        <a href=""
+                                           class="btn btn-sm btn-info me-1">
+                                            <i class="fa-solid fa-circle-info me-2"></i>Details
+                                        </a>
+                                        <a href=""
+                                           class="btn btn-sm btn-warning me-1">
+                                            <i class="fa-solid fa-pen-to-square me-2"></i>Edit
+                                        </a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
                     </table>
                 </div>
             </div>
