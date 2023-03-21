7e274a586f56b06bf26897115651787c7603ebd1
Alex Chiew <alex.chiewyz@gmail.com>
2023-03-22 00:35:01 +0800

    Get courses assigned to the instructor

--- app/Models/Instructor.php
+++ app/Models/Instructor.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Instructor extends Model
 {
@@ -43,5 +44,15 @@ class Instructor extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class, 'user_id');
+    }
+
+    /**
+     * Get the courses for the Instructor
+     *
+     * @return BelongsToMany
+     */
+    public function courses(): BelongsToMany
+    {
+        return $this->belongsToMany(Course::class, 'course_instructors', 'instructor_id', 'course_id');
     }
 }
