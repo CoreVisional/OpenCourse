7208c3bb3b22387e917bfae82534763947b6ea2c
Alex Chiew <alex.chiewyz@gmail.com>
2023-03-22 01:10:17 +0800

    Add some components to public homepage

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,15 @@
 
 @section('content')
 
-    {{-- TODO: Create structure of homepage here --}}
+    <div>
+        <img src="{{ asset('img/home/woman-writing.jpg') }}"
+             style="width: 100%; transform: scaleX(-1); position: relative" alt="Woman Concentrated on Writing">
+        <div style="width: 40%; position: absolute; top: 0; margin: 350px 0 0 100px;">
+            <h3>Empower Your Future with Free and Recognized Courses</h3>
+            <p>Take control of your career growth with access to top-notch and industry-recognized courses - all
+                available for free on OpenCourse</p>
+            <a href="{{ route('public.courses.index') }}" class="btn btn-outline-dark">Start Learning Now</a>
+        </div>
+    </div>
 
 @endsection
