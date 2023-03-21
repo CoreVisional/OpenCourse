f0a22d03b6792c15aad27b024c96a3006c6e475a
Alex Chiew <alex.chiewyz@gmail.com>
2023-03-22 02:28:42 +0800

    Add CourseController for instructor user

---
+++ app/Http/Controllers/Backend/Instructor/CourseController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers\Backend\Instructor;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\Request;
+
+class CourseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return View|Application|Factory
+     */
+    public function index(): View|Application|Factory
+    {
+        return view('backend.instructor.courses.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
