45ca3137e2af9d39ad13821a25a5b7ffb2b98399
Alex Chiew <alex.chiewyz@gmail.com>
2023-03-22 00:39:46 +0800

    Set login response to route to specific homepage based on the role of the logged in user

--- app/Http/Responses/LoginResponse.php
+++ app/Http/Responses/LoginResponse.php
@@ -12,7 +12,7 @@ class LoginResponse implements LoginResponseContract
     /**
      * Create an HTTP response that represents the object.
      *
-     * @param  Request  $request
+     * @param  Request $request
      * @return Response
      */
     public function toResponse($request): Response
